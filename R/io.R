efbe4556c865a386b9e42ccd940081d6c70fb284
Ben Tupper <btupper@bigelow.org>
2022-02-09 08:22:53 -0500

    add query_layers and compose_filename

--- R/io.R
+++ R/io.R
@@ -18,9 +18,12 @@ read_layers <- function(scenario = c('RCP45', 'RCP85', 'PRESENT')[1],
                        path = get_path("nc"),
                        verbose = FALSE){
   
-  filename <- switch(toupper(scenario[1]),
-      "PRESENT" = file.path(path, "PRESENT.nc"),
-      file.path(path, sprintf("%s_%s.nc", scenario[1], as.character(year[1]))))
+  filename <- compose_filename(scenario = scenario,
+                               year = year,
+                               path = path)
+  #filename <- switch(toupper(scenario[1]),
+  #    "PRESENT" = file.path(path, "PRESENT.nc"),
+  #    file.path(path, sprintf("%s_%s.nc", scenario[1], as.character(year[1]))))
   
   if (tolower(interval[1]) == 'ann'){
     # if all ann then read as attributes (one layer each)
@@ -38,4 +41,26 @@ read_layers <- function(scenario = c('RCP45', 'RCP85', 'PRESENT')[1],
   }
  x
 }
-                       
+
+
+#' Retrieve a listing of the variables in a Brickman file
+#' 
+#' @export
+#' @param scenario character, (case insensitive) one of 'RCP45', 'RCP85', or 'PRESENT'
+#' @param year integer or character, the year to read, ignored if scenario is 'PRESENT'
+#' @param path character, the root path to the data
+#' @return character vector of variable names
+query_layers <- function(scenario = c('RCP45', 'RCP85', 'PRESENT')[1],
+                         year = c(2055, 2075, NA)[1],
+                         path = get_path("nc")){
+  
+  filename <- compose_filename(scenario = scenario,
+                               year = year,
+                               path = path)
+  
+  X <- ncdf4::nc_open(filename)
+  vars <- names(X$var)
+  ncdf4::nc_close(X)
+  
+  vars
+}
--- R/path.R
+++ R/path.R
@@ -2,8 +2,28 @@
 #' 
 #' @export
 #' @param ... file path segements, see \code{\link[base]{file.path}}
-#' @param path character the root directory
+#' @param root character the root directory
 #' @return path description for a directory or file
 get_path <- function(..., root = "/mnt/ecocast/coredata/brickman"){
   file.path(root, ...)
+}
+
+
+#' Compose a Brickman filepath
+#' 
+#' @export
+#' @param scenario character, (case insensitive) one of 'RCP45', 'RCP85', or 'PRESENT'
+#' @param year integer or character, the year to read, ignored if scenario is 'PRESENT'
+#' @param path charcater, the root path to the data
+#' @examples 
+#' \dontrun{
+#' read_layers()
+#' }
+compose_filename <- function(scenario = c('RCP45', 'RCP85', 'PRESENT')[1],
+                        year = c(2055, 2075, NA)[1],
+                        path = get_path("nc")){
+  
+  switch(toupper(scenario[1]),
+         "PRESENT" = file.path(path, "PRESENT.nc"),
+         file.path(path, sprintf("%s_%s.nc", scenario[1], as.character(year[1]))))
 }
